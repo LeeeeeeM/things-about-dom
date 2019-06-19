f99223fbc40a54ffd3edcb77c1dc78140724c6c3
LeeeeeeM <sunliangmu@xiaomi.com>
2019-06-19 09:56:34 +0800

    fix v1.0 hook

--- src/pages/VueDataCons.vue
+++ src/pages/VueDataCons.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="vue-data">
+    <code>
+      {{code}}
+    </code>
     <div class="button-group">
       <div class="button-test" @click="loadVue('1.0.28')">加载1.0.28</div>
       <div class="button-test" @click="loadVue('2.5.3')">加载2.5.3</div>
@@ -14,36 +17,78 @@ const vueVersionMap = {
   '2.6.1': 'https://cdn.bootcss.com/vue/2.6.1/vue.js'
 }
 
-function LoadVue (version) {
+function loadSpecificVersionVue (version) {
   const script = document.createElement('script')
   script.src = vueVersionMap[version]
   document.head.appendChild(script)
   return new Promise(function (resolve, reject) {
-    script.onload = function (e) {
-      console.log(e)
-      resolve()
+    script.onload = function () {
+      window[`${version}-Vue`] = window.Vue
+      resolve(window[`${version}-Vue`])
+    }
+    script.onerror = function (e) {
+      reject(e)
     }
   })
 }
 
 export default {
-  mounted () {
-    this.$$_appDom = document.getElementById('vue-data')
-  },
-  methods: {
-    loadVue (version) {
-      return LoadVue(version).then(() => {
-        const div = document.createElement('div')
-        this.$$_appDom.appendChild(div)
-        new window.Vue({
-          template: `<div><div v-if="loading">{{x}}</div>
+  data () {
+    return {
+      code: `new Vue({
+          template: <div><div v-if="loading">{{x}}</div>
             <div>{{name}}</div>
-            <button @click="clickEvent">点击${version}</button></div>`,
+            <div @click="clickEvent">点击$\{version}</div></div>,
           data () {
             return {
               loading: false,
               name: ''
             }
+          },
+          mounted () {
+            this.deal()
+          },
+          ready () {
+            this.deal()
+          },
+          methods: {
+            deal () {
+              this.loading = false
+              const n = (Math.random() * 1000) >> 0
+              Promise.resolve().then(() => {
+                this.loading = true
+                this.name = n
+              }).then(() => {
+                this.x = n
+              })
+            },
+            clickEvent () {
+              this.deal()
+            }
+          }
+        })`
+    }
+  },
+  mounted () {
+    this.$$_appDom = document.getElementById('vue-data')
+  },
+  methods: {
+    loadVue (version) {
+      return loadSpecificVersionVue(version).then(Vue => {
+        const div = document.createElement('div')
+        this.$$_appDom.appendChild(div)
+        new Vue({
+          template: `<div><div v-if="loading">{{x}}</div>
+            <div>{{name}}</div>
+            <div @click="clickEvent" class="button-test">点击${version}</div></div>`,
+          data () {
+            return {
+              loading: false,
+              name: ''
+            }
+          },
+          ready () {
+            this.deal()
           },
           mounted () {
             this.deal()
@@ -64,6 +109,8 @@ export default {
             }
           }
         }).$mount(div)
+      }).catch(e => {
+        console.log(e)
       })
     }
   }
@@ -72,16 +119,16 @@ export default {
 <style lang="less">
   .button-group {
     text-align: left;
-    .button-test {
-      display: inline-block;
-      height: 40px;
-      padding: 0 10px;
-      margin-top: 20px;
-      line-height: 40px;
-      border-radius: 10px;
-      background: cadetblue;
-      color: #fff;
-      cursor: pointer;
-    }
+  }
+  .button-test {
+    display: inline-block;
+    height: 40px;
+    padding: 0 10px;
+    margin-top: 20px;
+    line-height: 40px;
+    border-radius: 10px;
+    background: cadetblue;
+    color: #fff;
+    cursor: pointer;
   }
 </style>
